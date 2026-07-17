7792b4ed970b44b1070b8ed9065dc29638eb9d79
plgreenLIRU <p.l.green@liverpool.ac.uk>
2026-07-17 10:39:01 +0100

    changing plot

--- post_process.py
+++ post_process.py
@@ -6,16 +6,49 @@ from Bayes import threshold_line
 df = pd.read_csv("population_samples_gold_standard.csv")
 
 plt.figure()
-plt.hist2d(df["pd"], df["pb"], bins=50, cmap="Blues", vmin=0)
 
-pd_range = np.linspace(0, 0.999, 100)
-pb_range = threshold_line(pd_range)
+# Plot limits
+xmax = 0.5
+ymax = 1.0
 
-plt.plot(pd_range, pb_range, color='black')
-plt.colorbar()
+# Threshold line
+pd_range = np.linspace(0, xmax, 500)
+pb_range = np.clip(threshold_line(pd_range), 0, ymax)
+
+# Background shading
+plt.fill_between(
+    pd_range, 0, pb_range,
+    color="green", alpha=0.3, zorder=0
+)
+plt.fill_between(
+    pd_range, pb_range, ymax,
+    color="red", alpha=0.3, zorder=0
+)
+
+# Copy the colormap so we can modify it
+cmap = plt.cm.Blues.copy()
+
+# Make values below vmin (i.e. zero-count bins) transparent
+cmap.set_under((1, 1, 1, 0))
+
+# 2D histogram
+plt.hist2d(
+    df["pd"],
+    df["pb"],
+    bins=50,
+    range=[[0, xmax], [0, ymax]],
+    cmap=cmap,
+    vmin=0.5,          # Zero-count bins are "under"
+    zorder=1
+)
+
+# Threshold line
+plt.plot(pd_range, pb_range, color="black", linewidth=2, zorder=2)
+
+plt.colorbar(label="Count")
 plt.xlabel(r"$p_d$")
 plt.ylabel(r"$p_b$")
-plt.xlim([0, 0.5])
-plt.ylim([0, 1])
+plt.xlim(0, xmax)
+plt.ylim(0, ymax)
 
 plt.show()
